dc89a9ed8e221f41dea6fdc4e473a1d7a63e59f2
James Fox-Collis <jlfoxcollis@gmail.com>
2021-02-12 10:32:54 -0700

    feature test 100%

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -8,7 +8,10 @@ describe 'it can search the airbender api' do
       click_on "Search For Members"
       expect(current_path).to eq(search_path)
 
-
+      expect(page).to have_content("Total Fire Nation Citizens: 97")  
+      expect(page.all('tr').count).to eq(26)
+      expect(page.all('tr')[0]).to have_content("Name")
+      expect(page.all('tr')[1]).to have_content("Afiko")
 
     end
 
