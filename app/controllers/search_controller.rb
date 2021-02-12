805d13c836478a5d19de055bed2615ca88498b19
James Fox-Collis <jlfoxcollis@gmail.com>
2021-02-12 10:01:51 -0700

    feature test setup

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,5 @@ class SearchController < ApplicationController
 
   def index
     @members = SearchFacade.search_by_nation(params[:nation])
-    binding.pry
   end
 end
--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -4,10 +4,11 @@ describe 'it can search the airbender api' do
   describe 'for fire nation clansfolk' do
     it 'will return list of airbender' do
       visit "/"
+      select "Fire Nation", from: "nation"
+      click_on "Search For Members"
+      expect(current_path).to eq(search_path)
 
-     first('#select_id option').select_option
 
-     expect(current_path).to eq(search_path)
 
     end
 
