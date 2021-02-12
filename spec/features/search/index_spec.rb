e42db0889df7db5ff0e142d412e988db87ce91a0
James Fox-Collis <jlfoxcollis@gmail.com>
2021-02-12 10:55:58 -0700

    updated search index header

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -8,7 +8,7 @@ describe 'it can search the airbender api' do
       click_on "Search For Members"
       expect(current_path).to eq(search_path)
 
-      expect(page).to have_content("Total Fire Nation Citizens: 97")  
+      expect(page).to have_content("Total Citizens: 97")  
       expect(page.all('tr').count).to eq(26)
       expect(page.all('tr')[0]).to have_content("Name")
       expect(page.all('tr')[0]).to have_content("Allies")
